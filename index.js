00c26b0a782f7376ead57fcd17bf4ce358b8dd4b
Nithin Ambati <nithinambati@Nithins-MacBook-Air.local>
2024-06-09 17:16:00 +0530

    DONE

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = 8000;
+
+const startServer = () => {
+  app.listen(PORT, () => {
+    console.log(`Server Running on Port NO: ${PORT}`);
+  });
+};
+
+startServer();
+
+mongoose
+  .connect("mongodb://localhost:27017/")
+  .then(() => console.log("MongoDB connected successfully"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+// Define User schema
+const userSchema1 = new mongoose.Schema({
+  username: String,
+  password: String,
+});
+
+const User = mongoose.model("user-logins", userSchema1);
+
+app.get("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const newUser = await new User({ username, password });
+  newUser.save();
+  res.status(200).send("uploaded");
+});
